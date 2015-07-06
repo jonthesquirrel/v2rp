56daa16f35971ad7b860291811c2d7ea7280fc0b
Lasercar7 <lasercar@users.noreply.github.com>
2015-07-06 14:31:23 -0500

    top bar + app resetter

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 
-var app = {};
+var temp_app = (function () {
+  var app = {};
 
-app.checkSupport = function () {
-  return (window.File && window.FileReader);
-};
+  app.checkSupport = function () {
+    return (window.File && window.FileReader);
+  };
 
-app.load = function () {
+  app.reset = function () {
+    localStorage.clear();
+    location.reload();
+  };
 
+  return app;
+})();
+
+load = function () {
+  app = temp_app;
   document.querySelector('.appstatus').innerHTML = app.checkSupport() ? 'Loaded' : 'Your browser does not support the required HTML5 APIs, try an updated version of Chrome or Firefox';
 };
 
-window.addEventListener('load', app.load);
+window.addEventListener('load', load);
