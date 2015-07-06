ebf25e7951c1967fbda811bf10961db8ab6f1745
Lasercar7 <lasercar@users.noreply.github.com>
2015-07-06 15:03:00 -0500

    more organization

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var temp_app = (function () {
   var app = {};
 
   app.checkSupport = function () {
-    return (window.File && window.FileReader);
+    return (window.File && window.FileReader && window.FileList && window.Blob);
   };
 
   app.reset = function () {
@@ -16,7 +16,7 @@ var temp_app = (function () {
 
 load = function () {
   app = temp_app;
-  document.querySelector('.appstatus').innerHTML = app.checkSupport() ? 'Loaded' : 'Your browser does not support the required HTML5 APIs, try an updated version of Chrome or Firefox';
+  document.querySelector('.appstatus').innerHTML = app.checkSupport() ? 'Loaded' : 'Your browser does not support the required HTML5 APIs, try an updated version of Chrome or Firefox.';
 };
 
 window.addEventListener('load', load);
